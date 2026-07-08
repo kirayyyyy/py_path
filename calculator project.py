ac813cea18ec1e3eb87dc5de27cd01ad9cc11a68
kirayyyyy <kirayom3@gmail.com>
2026-07-08 17:18:05 +0100

    Update calculator project.py

--- calculator project.py
+++ calculator project.py
@@ -11,21 +11,16 @@ def divide(a,b):
 
 choice = int(input("1. Add\n2. Substract\n3. Multiply\n4. Divide\nChoose a number:"))
 
-if choice == 1:
+if choice in [1,2,3,4]:
     fn = int(input("First number:"))
     fs = int(input("Second number:"))
-    print("result:",sum(fn,fs))
-elif choice == 2:
-    fn = int(input("First number:"))
-    fs = int(input("Second number:"))
-    print("result:",substract(fn,fs))
-elif choice == 3:
-    fn = int(input("First number:"))
-    fs = int(input("Second number:"))
-    print("result:", multiply(fn,fs))
-elif choice == 4:
-    fn = int(input("First number:"))
-    fs = int(input("Second number:"))
-    print("result:", divide(fn,fs))
+    if choice == 1:
+        print("result:",sum(fn,fs))
+    elif choice == 2:
+        print("result:",substract(fn,fs))
+    elif choice == 3:
+        print("result:", multiply(fn,fs))
+    elif choice == 4:
+        print("result:", divide(fn,fs))
 else:
     print("error try again")
